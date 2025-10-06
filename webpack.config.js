8cd764989167aa2097baf3873a59ecbf5e72b86f
pikloo <p.loukakou@gmail.com>
2025-10-06 18:59:00 +0200

    webpack fix

--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,32 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
+
   entry: {
     admin: './admin/js/genius-reviews-admin.js',
     public: './public/js/genius-reviews-public.js',
   },
+
   output: {
-    filename: 'genius-reviews-[name].bundle.js',
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname),
+    filename: (pathData) => {
+      if (pathData.chunk.name === 'admin') {
+        return 'admin/build/genius-reviews-admin.bundle.js';
+      }
+      if (pathData.chunk.name === 'public') {
+        return 'public/build/genius-reviews-public.bundle.js';
+      }
+      return '[name].js';
+    },
   },
-   module: {
+
+  module: {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'postcss-loader',
-        ],
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
     ],
   },
 };
+
